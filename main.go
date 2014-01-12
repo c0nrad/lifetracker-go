b4a8520ac175955a1ad839f6342e665f9ced366e
c0nrad <sclarsen@mtu.edu>
2014-01-11 21:27:56 -0500

    LifeTracker init

---
+++ main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"labix.org/v2/mgo"
+	"labix.org/v2/mgo/bson"
+	"net/http"
+)
+
+type Accomplishment struct {
+	ID   bson.ObjectId `bson:"_id,omitempty"`
+	Body string
+	Name string
+}
+
+var session *mgo.Session
+var templates = template.Must(template.ParseFiles("./templates/base.html", "./templates/index.html", "./templates/accomplishment.html", "./templates/addAccomplishment.html"))
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("Serving / %v\n", r.Method)
+	recent := findRecent()
+
+	err := templates.ExecuteTemplate(w, "base.html", recent)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func accomplishmentHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Serving", r.URL, r.Method, "\n")
+
+	if r.Method == "POST" {
+		r.ParseForm()
+		fmt.Println(r.Form, r.Form["accomplishment"])
+		c := session.DB("test").C("accomplishments")
+		err := c.Insert(&Accomplishment{"", r.Form["accomplishment"][0], r.Form["name"][0]})
+		if err != nil {
+			panic(err)
+		}
+	} else {
+
+	}
+	fmt.Fprintf(w, "Good Work!")
+}
+
+func initMGO() *mgo.Session {
+	session, err := mgo.Dial("127.0.0.1:27017")
+	if err != nil {
+		panic(err)
+	}
+
+	session.SetMode(mgo.Monotonic, true)
+
+	return session
+}
+
+func findRecent() []Accomplishment {
+	var results []Accomplishment
+	err := session.DB("test").C("accomplishments").Find(bson.M{}).All(&results)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return results
+}
+
+func main() {
+	session = initMGO()
+	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/a/", accomplishmentHandler)
+	fmt.Println("Starting server on port :7776")
+	http.ListenAndServe(":7776", nil)
+}
---
+++ setup.sh
@@ -0,0 +1,3 @@
+#! /usr/bin/fish
+set -x GOROOT /usr/lib/go
+set -x GOPATH /home/c0nrad/Projects/lifetracker-go
